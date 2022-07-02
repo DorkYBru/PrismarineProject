6399090b0c3fcbb5bf5494b9a483e5a6114fbad6
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-07-02 22:20:19 +0000

    ez

---
+++ scripts/smoothscroll/script.js
@@ -0,0 +1,31 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+    }
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    } else {
+        reveals[i].classList.remove("active");
+    }
+
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 150;
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        }
+    }
+}
+
+function scrollWin(x, y) {
+    window.scrollBy(x, y);
+}
